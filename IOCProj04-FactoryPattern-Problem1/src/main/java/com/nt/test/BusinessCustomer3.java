6880efd028e55c80f732b731d80fe3d898c79599
Lenovo <Lenovo@Lenovo-pc.LENOVO-PC>
2022-01-06 11:20:38 +0530

    IOCProj4-FactoryPatternProblem

---
+++ IOCProj04-FactoryPattern-Problem1/src/main/java/com/nt/test/BusinessCustomer3.java
@@ -0,0 +1,14 @@
+package com.nt.test;
+
+import com.nt.comp.BudgetCar;
+import com.nt.comp.Car;
+import com.nt.comp.LuxoryCar;
+
+public class BusinessCustomer3 {
+
+	public static void main(String[] args) {
+		Car car = new LuxoryCar("A234446");
+		car.drive();
+	}
+
+}
---
+++ IOCProj04-FactoryPattern-Problem1/src/main/java/com/nt/test/ProfessionalCustomer1.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+import com.nt.comp.BudgetCar;
+import com.nt.comp.Car;
+
+public class ProfessionalCustomer1 {
+
+	public static void main(String[] args) {
+		Car car = new BudgetCar("Yfhghj26587");
+		car.drive();
+	}
+
+}
